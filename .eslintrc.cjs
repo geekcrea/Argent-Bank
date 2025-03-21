f8cf517ca0edbe211bc825feeffe2655a2da92fc
geekcrea <webcrea.a@gmail.com>
2025-03-21 01:40:10 +0100

    ✨ Setup project with Vite, HTML structure, and ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,jsx,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ['react'],
+    rules: {},
+};
